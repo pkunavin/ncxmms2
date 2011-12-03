5ebdb53c7b9698e8bde29ce4a1be47c6107bbb78
Pavel Kunavin <tusk.kun@gmail.com>
2011-12-03 22:16:26 +0600

    lib: Add LineEdit widget.

---
+++ src/lib/lineedit.cpp
@@ -0,0 +1,237 @@
+/**
+ *  This file is a part of ncxmms2, an XMMS2 Client.
+ *
+ *  Copyright (C) 2011 Pavel Kunavin <tusk.kun@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ */
+
+#include <string>
+#include <glib.h>
+
+#include "lineedit.h"
+#include "painter.h"
+#include "application.h"
+
+namespace ncxmms2 
+{
+	class LineEditPrivate
+	{
+	public:
+		LineEditPrivate(LineEdit *q_) : q(q_), cursorPosition(0), viewportBegin(0) {}
+		
+		const LineEdit *q;
+		
+		LineEdit::ResultCallback resultCallback;
+		
+		std::wstring text;
+		std::wstring::size_type cursorPosition;
+		std::wstring::size_type viewportBegin;
+		typedef std::wstring::size_type TextSizeType;
+		
+		void returnResult(LineEdit::ResultCode result);
+		
+		void keyLeft();
+		void keyRight();
+		void keyHome();
+		void keyEnd();
+		void keyBackspace();
+		void keyDelete();
+		
+		void addChar(wchar_t ch);
+		
+		static std::wstring utf8ToWString(const std::string& str)
+		{
+			std::wstring result;
+			if (!g_utf8_validate(str.c_str(), str.size(), NULL))
+				return result;
+			
+			result.reserve(str.size());
+			
+			const char *c_str=str.c_str();
+			while (*c_str) {
+				result.push_back(g_utf8_get_char(c_str));
+				c_str=g_utf8_next_char(c_str);
+			}
+			return result;
+		}
+		
+		static std::string wStringToUtf8(const std::wstring& str)
+		{
+			std::string result;
+			result.reserve(str.size()*3);
+			
+			char buf[6];
+			int n;
+			for (wchar_t ch : str) {
+				n=g_unichar_to_utf8(ch, buf);
+				result.append(buf, n);
+			}
+			
+			return result;
+		}
+	};
+}
+
+using namespace ncxmms2;
+
+void LineEditPrivate::returnResult(LineEdit::ResultCode result)
+{
+	if (!resultCallback.empty())
+		resultCallback(wStringToUtf8(text), result);
+	Application::releaseFocus();
+}
+
+void LineEditPrivate::keyLeft()
+{
+	if (cursorPosition > 0) {
+		if (cursorPosition == viewportBegin)
+			--viewportBegin;
+		--cursorPosition;
+	}
+}
+
+void LineEditPrivate::keyRight()
+{
+	if (cursorPosition < text.size()) {
+		if (cursorPosition-viewportBegin == (TextSizeType)q->cols()-1)
+			++viewportBegin;
+		++cursorPosition;
+	}
+}
+
+void LineEditPrivate::keyHome()
+{
+	cursorPosition=0;
+	viewportBegin=0;
+}
+
+void LineEditPrivate::keyEnd()
+{
+	cursorPosition=text.size();
+	if (text.size() >= (TextSizeType)q->cols())
+		viewportBegin=text.size() - q->cols() + 1;
+}
+
+void LineEditPrivate::keyBackspace()
+{
+	if (cursorPosition > viewportBegin) {
+		if (viewportBegin + q->cols() >= text.size()) {
+			if (viewportBegin > 0) {
+				--viewportBegin;
+			}
+		}
+	} else {
+		if (viewportBegin > 0) {
+			--viewportBegin;
+		} else {
+			return;
+		}
+	}
+	--cursorPosition;
+	text.erase(cursorPosition, 1);
+}
+
+void LineEditPrivate::keyDelete()
+{
+	if (cursorPosition < text.size()) {
+		++cursorPosition;
+		keyBackspace();
+	}
+}
+
+void LineEditPrivate::addChar(wchar_t ch)
+{
+	if (cursorPosition < text.size()) {
+		text.insert(cursorPosition, 1, ch);
+	} else {
+		text.push_back(ch);
+	}
+	
+	if (cursorPosition-viewportBegin == (TextSizeType)q->cols()-1)
+		++viewportBegin;
+	++cursorPosition;
+}
+
+LineEdit::LineEdit(int cols, int yPos, int xPos, Window* parent) :
+	Window(1, cols, yPos, xPos, parent),
+	d(new LineEditPrivate(this))
+{
+	
+}
+
+void LineEdit::edit(const ResultCallback& resultCallback, const std::string& text)
+{
+	edit(resultCallback, LineEditPrivate::utf8ToWString(text));
+}
+
+void LineEdit::edit(const ResultCallback& resultCallback, const std::wstring& text)
+{
+	d->resultCallback=resultCallback;
+	d->text=text;
+	d->cursorPosition=0;
+	Application::stealFocus(this);
+	update();
+}
+
+void LineEdit::keyPressedEvent(const KeyEvent& keyEvent)
+{
+	if (keyEvent.isFunctionKey()) {
+		switch(keyEvent.key()) {
+			case KeyEvent::KeyEscape:    d->returnResult(Rejected); return;
+			case KeyEvent::KeyEnter:     d->returnResult(Accepted); return;
+			case KeyEvent::KeyLeft:      d->keyLeft();              break;
+			case KeyEvent::KeyRight:     d->keyRight();             break;
+			case KeyEvent::KeyHome:      d->keyHome();              break;
+			case KeyEvent::KeyEnd:       d->keyEnd();               break;
+			case KeyEvent::KeyBackspace: d->keyBackspace();         break;
+			case KeyEvent::KeyDelete:    d->keyDelete();            break;
+		}
+	} else if (g_unichar_isprint(keyEvent.key())) {
+		d->addChar(keyEvent.key());
+	}
+	update();
+}
+
+void LineEdit::resizeEvent(const Size &size)
+{
+	if (size.cols() > cols()) {
+		int extraSize=size.cols()-cols();
+		if (d->viewportBegin + cols() < d->text.size())
+			extraSize-=d->text.size() - (d->viewportBegin + cols());
+		if (extraSize > 0) {
+			if ((LineEditPrivate::TextSizeType)extraSize > d->viewportBegin) {
+				d->viewportBegin=0;
+			} else {
+				d->viewportBegin-=extraSize;
+			}
+		}
+	} else {
+		int sizeDiff=cols()-size.cols();
+		sizeDiff-=cols() - (d->cursorPosition - d->viewportBegin + 1);
+		if (sizeDiff > 0)
+			d->viewportBegin+=sizeDiff;
+	}
+	
+	Window::resizeEvent(size);
+}
+
+void LineEdit::showEvent()
+{
+	Painter painter(this);
+	painter.clearLine();
+	painter.printString(d->text.c_str() + d->viewportBegin, cols());
+	const auto cursorPosition=d->cursorPosition;
+	painter.move(cursorPosition - d->viewportBegin, 0);
+	painter.setReverse(true);
+	painter.printChar(cursorPosition < d->text.size() ? d->text[cursorPosition] : wchar_t(' '));
+	painter.flush();
+}
---
+++ src/lib/lineedit.h
@@ -0,0 +1,52 @@
+/**
+ *  This file is a part of ncxmms2, an XMMS2 Client.
+ *
+ *  Copyright (C) 2011 Pavel Kunavin <tusk.kun@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ */
+
+#ifndef LINEEDIT_H
+#define LINEEDIT_H
+
+#include "window.h"
+
+namespace ncxmms2 
+{
+	class LineEditPrivate;
+	
+	class LineEdit : public Window
+	{
+	public:
+		LineEdit(int cols, int yPos, int xPos, Window *parent=0);
+		
+		enum ResultCode
+		{
+			Rejected,
+			Accepted
+		};
+		typedef boost::function<void (const std::string&, ResultCode)> ResultCallback;
+				
+		void edit(const ResultCallback& resultCallback, const std::string& text=std::string());
+		void edit(const ResultCallback& resultCallback, const std::wstring& text=std::wstring());
+			
+		virtual void keyPressedEvent(const KeyEvent& keyEvent);
+		virtual void resizeEvent(const Size& size);
+		
+	protected:
+		virtual void showEvent();
+		
+	private:
+		std::unique_ptr<LineEditPrivate> d;
+	};
+}
+
+#endif // LINEEDIT_H
