f6090a115a4a03305c2dd406ebd5825634575c1f
Pavel Kunavin <tusk.kun@gmail.com>
2013-04-13 21:45:09 +0600

    Utils: Add format function.
    Adds simple boost::format wrapper.

--- src/utils.h
+++ src/utils.h
@@ -18,6 +18,8 @@
 #define UTILS_H
 
 #include <string>
+#include <functional>
+#include <boost/format.hpp>
 
 namespace ncxmms2 {
 namespace Utils {
@@ -32,6 +34,53 @@ enum FileType
 };
 
 FileType getFileType(const std::string& path);
+
+namespace FormatImpl {
+
+class BoostFormatWrapper
+{
+    boost::format m_format;
+
+    template <typename T, typename Arg>
+    auto applyArgs(T&& value, Arg&& arg) -> decltype(std::forward<T>(value) % std::forward<Arg>(arg))
+    {
+        return std::forward<T>(value) % std::forward<Arg>(arg);
+    }
+
+    template <typename T, typename Arg, typename... Args>
+    auto applyArgs(T&& value, Arg&& arg,  Args&&... args)
+    -> decltype(applyArgs(std::forward<T>(value) % std::forward<Arg>(arg),
+                          std::forward<Args>(args)...))
+    {
+        return  applyArgs(std::forward<T>(value) % std::forward<Arg>(arg),
+                          std::forward<Args>(args)...);
+    }
+
+public:
+    template <typename T>
+    BoostFormatWrapper(T&& str) :
+        m_format(std::forward<T>(str)) {}
+
+    const boost::format& compile()
+    {
+        return m_format;
+    }
+
+    template <typename... Args>
+    auto compile(Args&&... args) -> decltype(applyArgs(m_format, std::forward<Args>(args)...))
+    {
+        return applyArgs(m_format, std::forward<Args>(args)...);
+    }
+};
+} // FormatImpl
+
+// Convenient wrapper around boost::format
+// Replaces boost::format(string) % arg_1 % arg_2 ...  to format(string, arg_1, arg_2 ...)
+template <typename T, typename... Args>
+std::string format(T&& str, Args&&... args)
+{
+    return FormatImpl::BoostFormatWrapper(std::forward<T>(str)).compile(std::forward<Args>(args)...).str();
+}
 
 } // Utils
 } // ncxmms2
